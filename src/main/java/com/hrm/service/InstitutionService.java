57aad23c974101e880639cb8b9ebc506d1c2211f
yuhand <43294320+yuhand@users.noreply.github.com>
2020-06-22 08:49:54 +0800

    更新service_余轩

---
+++ src/main/java/com/hrm/service/InstitutionService.java
@@ -0,0 +1,38 @@
+package com.hrm.service;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.hrm.bean.ChangeDemo;
+import com.hrm.bean.InstitutionDemo;
+
+import java.util.Date;
+import java.util.List;
+
+public interface InstitutionService {
+
+
+    void insertInstitution(InstitutionDemo institutionDemo);
+
+    void updateInstitution(InstitutionDemo institutionDemo);
+
+    List<InstitutionDemo> queryAll();
+
+    InstitutionDemo queryByInsId(Integer INS_ID);
+
+    InstitutionDemo queryByInsName(String INS_NAME);
+
+    List<InstitutionDemo> queryByInsNomes(String INS_NOMES);
+
+    List<InstitutionDemo> queryByFinsId(String FINS_ID);
+
+
+
+    int deleteAll();
+
+    int deleteByInsId(Integer INS_ID);
+
+    int deleteByInsName(String INS_NAME);
+
+    int deleteByInsNomes(String INS_NOMES);
+
+    int deleteByFinsId(String FINS_ID);
+}
---
+++ src/main/java/com/hrm/service/PositionService.java
@@ -0,0 +1,48 @@
+package com.hrm.service;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.hrm.bean.ChangeDemo;
+import com.hrm.bean.InstitutionDemo;
+import com.hrm.bean.PositionDemo;
+
+import java.util.Date;
+import java.util.List;
+
+public interface PositionService {
+
+
+    //插入
+    void insertPosition(PositionDemo positionDemo);
+
+    //修改
+    void updatePosition(PositionDemo positionDemo);
+
+    /*
+    查找数据
+     */
+    //查询所有
+    List<PositionDemo> queryAll();
+    //员工编号
+    PositionDemo queryByPosEmpId(Integer EMP_ID);
+    //职位编号
+    PositionDemo queryByPosId(Integer POS_ID);
+    //职位名称
+    PositionDemo queryByPosName(String POS_NAME);
+    //职位职能
+    PositionDemo queryByPosFunc(String POS_FUNC);
+
+
+    /*
+    删除数据
+     */
+    //删除所有
+    int deleteAll();
+    //员工编号
+    int deleteByPosEmpId(Integer EMP_ID);
+    //职位编号
+    int deleteByPosId(Integer POS_ID);
+    //职位名称
+    int deleteByPosName(String POS_NAME);
+    //职位职能
+    int deleteByPosFunc(String POS_FUNC);
+}
